84f62283993809dd295f5f481e7414bbbd439072
Sheldon Lobo <sheldon.lobo@oracle.com>
2020-07-28 09:28:51 -0400

    * Moved the LabelImagePair class to a new java file * Added EMNIST test letter data to the repo

--- src/main/java/org/lobo/LabelImageData.java
+++ src/main/java/org/lobo/LabelImageData.java
@@ -5,36 +5,6 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-class LabelImagePair {
-    private int index;
-    private int label;
-    private int[][] image = new int[28][28];
-
-    public LabelImagePair(int index, int label, int[][] image) {
-        this.index = index;
-        this.label = label;
-
-        // Copy the 2d array
-        for (int i = 0; i < image.length; i++) {
-            for (int j = 0; j < image[i].length; j++) {
-                this.image[i][j] = image[i][j];
-            }
-        }
-    }
-
-    public int index() {
-        return index;
-    }
-
-    public int label() {
-        return label;
-    }
-
-    public int[][] image() {
-        return image;
-    }
-}
-
 public class LabelImageData {
     private ArrayList<LabelImagePair> data;
 
